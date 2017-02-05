9302f6dd2961b3724279716225ff4a6e886296ea
Florian Bouron <florianbouron@Florians-MacBook-Air.local>
2017-02-05 14:43:27 +0100

    Add last book to home

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
 
 def index(request):
-    return render(request, 'website/index.html', {})
+    last_books = Book.objects.all().order_by('-published_date')[:3]
+    return render(request, 'website/index.html', {'last_books': last_books})
